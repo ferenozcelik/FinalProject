78ca41a0fd5025f36df2b87dc91162b90b5b85a3
ferenozcelik <eren.ozcelik@std.antalya.edu.tr>
2021-02-14 23:55:40 +0300

    14.02.2021

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+        // constructor
+        public Result(bool success, string message) : this(success) // tek parametreli olan constructor'a success'i yolla.
+        {
+            Message = message;
+            // Success = success;   bu işlemi burada iptal edip alttaki constructor'a bıraktık.
+        }
+
+        // constructor
+        // işlemin gerçekleşip gerçekleşmediğine dair mesaj yok. sadece başarı durumu var.
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        
+        public string Message { get; }
+    }
+}
